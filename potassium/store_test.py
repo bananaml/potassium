be0cc93175084aeec69d2d1ff1f2ceb393af02b8
erik-dunteman <erikdunteman@gmail.com>
2023-06-21 17:57:39 -0700

    added s3 store

--- potassium/store_test.py
+++ potassium/store_test.py
@@ -1,4 +1,5 @@
-from store import Store, RedisConfig
+import os
+from store import Store, RedisConfig, S3Config
 
 # Redis store can save json serializeable objects
 store = Store(
@@ -49,6 +50,78 @@ store = Store(
     config=RedisConfig(
         host="localhost",
         port=6379,
+        encoding="pickle"
+    )
+)
+
+print("Testing pickle redis")
+for obj in objs:
+    store.set("key", obj)
+    out = store.get("key")
+    if obj != obj:
+        print("failed case", obj)
+
+
+# ----
+
+
+# s3
+access_key = os.environ["AWS_ACCESS_KEY_ID"]
+secret_access_key = os.environ["AWS_SECRET_ACCESS_KEY"]
+bucket = "potassium-test"
+
+
+# s3 store can save json serializeable objects
+store = Store(
+    backend="s3",
+    config=S3Config(
+        access_key=access_key,
+        secret_access_key=secret_access_key,
+        bucket=bucket,
+    )
+)
+
+objs = [
+    "1",
+    True,
+    ["some", "list"],
+    {"some": {"nested": "dict"}}
+]
+
+print("Testing json redis")
+for obj in objs:
+    store.set("key", obj)
+    out = store.get("key")
+    if out != obj:
+        print("failed case", obj)
+
+# ----
+
+# pickle s3 can save complex objects
+
+
+class Complex():
+    def __init__(self, a) -> None:
+        self.a = a
+
+    def __eq__(self, other):  # necessary for the equal assert later
+        return self.a == other.a
+
+
+objs = [
+    "1",
+    True,
+    ["some", "list"],
+    {"some": {"nested": "dict"}},
+    Complex(a=1)
+]
+
+store = Store(
+    backend="s3",
+    config=S3Config(
+        access_key=access_key,
+        secret_access_key=secret_access_key,
+        bucket=bucket,
         encoding="pickle"
     )
 )
