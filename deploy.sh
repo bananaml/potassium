101f4acc4044328bf9fc088a36abf80eb8cb843b
Aaron Peddle <peddle.aaron@gmail.com>
2023-10-27 11:07:18 -0700

    fix deploy script (#42)

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
-!#/bin/bash
+#! /bin/bash
 python3 setup.py sdist bdist_wheel
-python3 -m twine upload dist/* --skip-existing
+python3 -m twine upload dist/* --skip-existing -u $PYPI_USERNAME -p $PYPI_PASSWORD
